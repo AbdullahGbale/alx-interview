a03192fdce9fd0a717763f8853d369013dc810ed
AbdullahGbale <bigimarniac@gmail.com>
2024-10-10 17:54:37 -0700

    debugging

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" a method that determines if all the boxes can be opened """
+"""A method that determines if all the boxes can be opened."""
 
 
 def canUnlockAll(boxes):
-    """Function that takes a list of a list and the
-    contents of a list will unlock other lists
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
 
     keys = [0]
@@ -12,6 +12,6 @@ def canUnlockAll(boxes):
         for boxKey in boxes[key]:
             if boxKey not in keys and boxKey < len(boxes):
                 keys.append(boxKey)
-    return len(keys) == len(boxes)  #check
+    return len(keys) == len(boxes)  # More concise check
 
-# Remove whitespace
+# Remove the blank line with whitespace (if your editor adds it)
